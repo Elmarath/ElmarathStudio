8ae1533925bc1e4efd258977a0aca2b90db11a36
Denizhan <65414809+Elmarath@users.noreply.github.com>
2025-10-04 07:55:47 +0300

    glowing line added

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,22 @@ import HireBox from '../components/HireBox';
 import PortfolioBox from '../components/PortfolioBox';
 import HeaderBox from '../components/HeaderBox';
 
+// Simple animated connector with thin fading line
+function AnimatedConnector() {
+  return (
+    <div className={styles.connectorSection}>
+      {/* Simple thin fading line with center animation */}
+      <div className={styles.simpleLine}>
+        <div className={styles.lineLeft}></div>
+        <div className={styles.centerPulse}>
+          <div className={styles.pulseRing}></div>
+        </div>
+        <div className={styles.lineRight}></div>
+      </div>
+    </div>
+  );
+}
+
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
@@ -30,6 +46,7 @@ export default function Home() {
       title={`${siteConfig.title}`}
       description="Game and Game Tools Development">
       <HomepageHeader />
+      <AnimatedConnector />
       <main>
         <HomepageDocumentPreviews />
         <hr />
