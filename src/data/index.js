c722f7b5bee661b6cb02dc7f3e279cf5fd3b6251
Denizhan <65414809+Elmarath@users.noreply.github.com>
2025-11-22 02:36:39 +0300

    updated links

--- src/data/index.js
+++ src/data/index.js
@@ -8,7 +8,7 @@ export const DOCUMENT_LIST = [
     Img: require('@site/static/img/T_StagedInteractionThumbnail.png').default,
     description: "Staged Interaction System that uses stages as a way to create complex player interactions with ease.",
     DocLink: 'docs/Staged-Interaction-System/Overview',
-    FabLink: 'https://www.unrealengine.com/marketplace/en-US/product/staged-interaction-system',
+    FabLink: 'https://www.fab.com/listings/ea6cda8e-25b1-434e-a9ab-1c2bbaa5fe67',
   },
   {
     id: 'EasyTraceTrailSystem',
@@ -16,7 +16,7 @@ export const DOCUMENT_LIST = [
     Img: require('@site/static/img/T_EasyTraceTrailSystemThumbnail.png').default,
     description: "A modular, optimized solution for easily controlling and extending hit results from continuous hit trails.",
     DocLink: '/docs/Easy-Trace-Trail-System/EasyTraceTrailSystem',
-    FabLink: 'https://www.unrealengine.com/marketplace/en-US/product/advanced-hit-trailer',
+    FabLink: 'https://www.fab.com/listings/86d9af14-9ab3-4c47-83b2-96eb6a4b5957',
   },
   {
     title: "Ability Queue Task System",
@@ -30,21 +30,21 @@ export const DOCUMENT_LIST = [
     Img: require('@site/static/img/T_NIA_Thumbnail.png').default,
     description: "Create customizable, nested, section-based, object-oriented inventory systems for intuitive gameplay.",
     DocLink: '/docs/Nested-Invnetory-Architecture/AboutNestedInventoryArchitecture',
-    FabLink: 'https://www.unrealengine.com/marketplace/en-US/product/nested-inventory-architecture',
+    FabLink: 'https://www.fab.com/listings/320f157f-9241-47ee-9f32-63aaf744f509',
   },
   {
     title: "Modular Action System",
     Img: require('@site/static/img/T_ModularActionSystemLogo.png').default,
     description: "A modular, data-driven action system for games. Network-ready and easy to customize.",
     DocLink: '/docs/Modular-Action-System/About-Modular-Action-System',
-    FabLink: 'https://www.unrealengine.com/marketplace/en-US/product/modular-action-system',
+    FabLink: 'https://www.fab.com/listings/6ef3ecaa-1a2f-4056-a209-cf228df40c3f',
   },
   {
     title: "Easy Skeletal Mesh Merge Async",
     Img: require('@site/static/img/EasySkeletalMeshMergeAsync.png').default,
     description: "A simple and optimized workflow to create modular characters via skele merging.",
     DocLink: '/docs/category/easy-skeletal-mesh-merge-async',
-    FabLink: 'https://www.unrealengine.com/marketplace/en-US/product/easy-skeletal-mesh-merge-async',
+    FabLink: 'https://www.fab.com/listings/616906a0-c73d-4e7a-9002-866a6d9751d5',
     GitHubLink: 'https://github.com/Elmarath/EasySkeletalMeshMergeAsync',
   },
   {
@@ -52,14 +52,14 @@ export const DOCUMENT_LIST = [
     Img: require('@site/static/img/T_EasyAbilitySetSystem_Logo.png').default,
     description: "Ability Set Management System that handles both granting and input binding for Gameplay Ability System.",
     DocLink: '/docs/Easy-Ability-Set-System/EasyAbilitySetSystemStartup',
-    FabLink: 'https://www.unrealengine.com/marketplace/en-US/product/easy-ability-set-system',
+    FabLink: 'https://www.fab.com/sellers/Elmarath',
   },
   {
     title: "Dynamic Input System",
     Img: require('@site/static/img/T_DynamicInputSystemThumbanil.png').default,
     description: "A flexible, runtime input management system that allows dynamic binding and modification of input mappings during gameplay.",
     DocLink: '/docs/Dynamic-Input-System/DynamicInputSystem',
-    GitHubLink: 'https://github.com/Elmarath/DynamicInputSystem',
+    GitHubLink: 'https://www.fab.com/listings/cdf2ad8e-b9b7-4b4e-87bb-fdcfbe3c3b2b',
   }
 ];
 
